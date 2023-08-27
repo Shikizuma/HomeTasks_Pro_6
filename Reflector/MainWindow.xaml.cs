c55cf0b0f428d5e86add11f1ed7e1dfe53cd58bd
Miroslav <myroslav.bashkirov@gmail.com>
2023-08-27 19:38:54 +0300

    Add reflector

--- Reflector/MainWindow.xaml.cs
+++ Reflector/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,53 @@ namespace Reflector
     /// </summary>
     public partial class MainWindow : Window
     {
+        string selectedFilePath;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Title = "Оберіть файл";
+            openFileDialog.Filter = "Всі файли (*.*)|*.*|Текстові файли (*.txt)|*.txt";
+
+            if (openFileDialog.ShowDialog() == true)
+            {               
+                selectedFilePath = openFileDialog.FileName;
+            }
+
+            ShowAssemblyInfo();
+        }
+
+        private void ShowAssemblyInfo()
+        {
+            Assembly assembly = Assembly.LoadFrom(selectedFilePath);
+
+            InfoTextBox.Text += "Ім'я складання:" + assembly.FullName + Environment.NewLine + Environment.NewLine;
+            InfoTextBox.Text += $"Версія складання: {assembly.GetName().Version}" + Environment.NewLine;
+            InfoTextBox.Text += $"Культура складання: {assembly.GetName().CultureInfo.DisplayName}" + Environment.NewLine;
+
+            Type[] types = assembly.GetTypes();
+            InfoTextBox.Text += $"Кількість типів у складанні: {types.Length}" + Environment.NewLine;
+            InfoTextBox.Text += "Список типів:" + Environment.NewLine;
+
+            foreach (Type item in types)
+            {
+                InfoTextBox.Text += $"- {item.FullName}" + Environment.NewLine;
+
+                MethodInfo[] methods = item.GetMethods();
+                InfoTextBox.Text += $"  Кількість методів у типі {item.FullName}: {methods.Length}" + Environment.NewLine;
+                InfoTextBox.Text += "  Список методів:" + Environment.NewLine;
+                foreach (MethodInfo method in methods)
+                {
+                    InfoTextBox.Text += $"    - {method.Name}" + Environment.NewLine;
+                }
+            }
+        }
+
+
     }
 }
