06da429bd35fdb0342ca1f7973313b246849badf
Miroslav <myroslav.bashkirov@gmail.com>
2023-08-27 17:20:07 +0300

    Add project files.

---
+++ HomeTasks_Pro_6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using TemperatureConverterLibrary;
+
+namespace HomeTasks_Pro_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char degreeSymbol = '\u00B0';
+
+            Assembly assembly = Assembly.Load("TemperatureConverterLibrary");
+
+            Type type = assembly.GetType("TemperatureConverterLibrary.TemperatureConverter");
+
+            object obj = Activator.CreateInstance(type);
+
+            MethodInfo methodInfo = type.GetMethod("FahrenheitToCelsius");
+
+            Console.WriteLine($"32 {degreeSymbol}F == {methodInfo.Invoke(obj, new object[] { 32 })} {degreeSymbol}C");
+
+            methodInfo = type.GetMethod("CelsiusToFahrenheit");
+
+            Console.WriteLine($"32 {degreeSymbol}C == {methodInfo.Invoke(obj, new object[] { 32 })} {degreeSymbol}F");
+
+
+        }
+    }
+}
---
+++ TemperatureConverterLibrary/TemperatureConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace TemperatureConverterLibrary
+{
+    public class TemperatureConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+    }
+}
